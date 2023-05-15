08b5ae98bc3428bde2e828d372cb8bd9d4a2f612
IanStyler <ian.soles@institutvidreres.cat>
2023-05-15 23:23:37 +0200

    Initial commit

---
+++ src/Prova.java
@@ -0,0 +1,44 @@
+import javax.swing.*;
+
+public class Prova {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Decimal a binari");
+        JLabel decLabel = new JLabel("Numero en decimal a resoldre: ");
+        JLabel resultatDecimalBinari = new JLabel("RESOLTAT EN BINARI: ");
+        JTextField decField = new JTextField(20);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JPanel panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+        JLabel numBinariLabel = new JLabel("Numero en binari a resoldre: ");
+        JLabel resultatBinariDecimal = new JLabel("RESOLTAT EN DECIMAL: ");
+        JTextField binariAdecimal = new JTextField(20);
+
+        decField.setColumns(35);
+        binariAdecimal.setColumns(35);
+
+        decField.setMaximumSize(decField.getPreferredSize());
+        binariAdecimal.setMaximumSize(binariAdecimal.getPreferredSize());
+
+        panel.add(decLabel);
+        panel.add(decField);
+        panel.add(resultatDecimalBinari);
+        panel.add(numBinariLabel);
+        panel.add(binariAdecimal);
+        panel.add(resultatBinariDecimal);
+
+        decField.addActionListener(e -> {
+            String decStr = decField.getText();
+            try {
+                int dec = Integer.parseInt(decStr);
+                String bin = Integer.toBinaryString(dec);
+                resultatDecimalBinari.setText("Numero en binari: " + bin);
+            } catch (NumberFormatException ex) {
+                resultatDecimalBinari.setText("Numero decimal no vàlid!");
+            }
+        });
+
+        frame.add(panel);
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
